ea794ddc0a591e13633cb90ca1400b0aed4249dd
muirne <muirne22@hotmail.com>
2018-12-28 22:40:24 +0100

    new

--- js/zadanie.js
+++ js/zadanie.js
@@ -1,53 +1,3 @@
-// function myFunction() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
-//
-// function set_background(){
-//   var col = document.querySelector('cat');
-//   let para = document.querySelector('par');
-//   console.log(para);
-//   col.addEventListner("click", function () {
-//   para.style.background = "blue"
-// });
-// }
-//
-//
-// function Bill (string){
-//     var arra = string.split(" ");
-//     var arraynew = [];
-//     var arra_length = arra.length-1;
-//     for (var i = 0; i < 2^arra_length; i++) {
-//       arraynew = onezero(i,arra_length);
-//       var outcome= '';
-//       for (var j = 0; j < arra_length; j++) {
-//           if (arraynew[j]===1) {
-//               concat.outcome(arra[j]);
-//
-//           }
-//       }
-//       print(outcome);
-//     }
-//
-// }
-//
-// function onezero(number, length) {
-//     var total = number;
-//     var array = [];
-//     for (var i = number.length -1;i>=0; i--) {
-//       if (2^i<=total) { array.push(1);
-//          total= total - 2^i;
-//      }else{
-//          array.push(0);
-//      }
-//
-//     }
-//     return array;
-// }
 
 
 function yesnoCheck() {
@@ -87,94 +37,43 @@ function wheel(){
 }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    var form = document.querySelector('form');
-    var errors = document.querySelector('.error-message');
+$(document).ready(function() {
 
-    form.addEventListener("submit", function(e) {
-
-        e.preventDefault();
-
-        errors.innerHTML = "";
-
-        var valid = true;
-
-        if (valid) {
-                    form.submit();
-                }
-
-            })
-
-        });
-
-        heartAjax() {
-            let heart = document.querySelectorAll(".btn-primary");
-
-            // CLICKED_REF
-            let clickedHeart;
-            let on = true;
-
-              heart.addEventListener("click", function() {
-                clickedHeart = this;
-                ajax();
-              }, false);
-
-            // AJAX
-            function ajax() {
-              const url = "https://demo3451683.mockable.io/mag";
-              const request = new XMLHttpRequest();
-              request.open("GET", url);
-              request.onload = function() {
-                if (request.status === 200) {
-                  getResponse(this.responseText);
-                }
-              };
-              request.send(null);
-            }
-            // GET_RESPONSE
-            function getResponse(responseText) {
-              const response = JSON.parse(responseText);
-              if (on) {
-                alert(response.dodano);
-                on = null;
-              } else {
-                alert(response.niedodano);
-                on = true;
-              }
-              console.log("submit");;
+$(".form").submit(function(e) {
+    var formURL = $('form').attr('action');
+$.ajax({
+            type: 'POST',
+            url:formURL,
+            data: $(".form").serialize(),
+            success: function(data) {
+                console.log(data);
             }
 
 
-// document.addEventListener("DOMContentLoaded", function(){
-//     var buttonIcon = document.querySelector('.icon-wheel');
-//     buttonIcon.addEventListner("click", function(){
-//
-//         var wheels = document.querySelector(".wheelnr").value;
-//         if ( typeof wheels === "number" &&  wheels > 4){
-//             document.querySelector('.wheel').style.display = 'block'
-//         }else{
-//             document.querySelector('.wheel').style.display = 'none';
-//     }
-//
-// });
-// })
-//
-//     var form = document.querySelector('form');
-// document.addEventListener("DOMContentLoaded", function(){
-//
-//     var form = document.querySelector('form');
-//
-//
-//     form.addEventListener("submit", function(e){
-//
-//       e.preventDefault();
-//
+                    });
 
+
+
+
+e.preventDefault();
+})
+})
+// document.addEventListener("DOMContentLoaded", function() {
+//     var form = document.querySelector('form');
+//     var errors = document.querySelector('.error-message');
 //
-//                // var inputs = document.querySelectorAll('input[type=text]');
-//                // for (var i=0; i<inputs.length; i++) {
-//                //         inputs[i].value="";
-//                // }
+//     form.addEventListener("submit", function(e) {
 //
-//            });
-//  })
+//         e.preventDefault();
+//
+//         errors.innerHTML = "";
+//
+//         var valid = true;
+//
+//         if (valid) {
+//                     form.submit();
+//                 }
+//
+//             })
+//
+//         });
